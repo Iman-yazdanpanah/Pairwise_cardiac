a707ffe8dfcb0944ed2d8137ce74eb80a6d8f042
Iman-yazdanpanah <144800001+Iman-yazdanpanah@users.noreply.github.com>
2024-07-15 04:27:12 +0330

    Update custom_dataset.py

--- dataloaders/datasets/custom_dataset.py
+++ dataloaders/datasets/custom_dataset.py
@@ -6,7 +6,7 @@ from torch.utils.data import Dataset
 from torchvision import transforms
 
 from dataloaders import medical_transforms as tr
-
+    
 
 class CustomDataset(Dataset):
     def __init__(self, csv_file, image_dir, mask_dir, transform=None):
@@ -14,7 +14,7 @@ class CustomDataset(Dataset):
         self.image_dir = image_dir
         self.mask_dir = mask_dir
         self.transform = transform
-
+        self.data1_files = self.data['image_id'].tolist()  # Assuming the CSV file has a column 'image' with paths
     def __len__(self):
         return len(self.data)
 
@@ -24,15 +24,14 @@ class CustomDataset(Dataset):
         image = Image.open(img_name).convert('RGB')
         mask = Image.open(mask_name).convert('L')
 
-        sample = {'image': image, 'mask': mask}
+        sample = {'image': image, 'mask': mask }
 
         if self.transform:
             sample = self.transform(sample)
 
         return sample['image'], sample['mask'], idx
-
     def get_image_paths(self):
-        return [os.path.join(self.image_dir, self.data.iloc[i, 0] + '.png') for i in range(len(self.data))]
+        return self.data1_files
 
         
 crop_size=512
@@ -46,7 +45,8 @@ transform  = transforms.Compose([
 transforms.Resize((256, 256)),
 transforms.ToTensor()
 ])'''
-dataset = CustomDataset(csv_file='./train/train.csv', image_dir='./train/images', mask_dir='./train/masks', transform=transform)
+dataset = dataset = CustomDataset(csv_file='./train/train.csv', image_dir='./train/images', mask_dir='./train/masks', transform=transform)
+
 
 # Create a DataLoader
 #dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
